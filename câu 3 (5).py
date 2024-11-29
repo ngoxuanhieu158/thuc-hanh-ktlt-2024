99910378f383a4009c2f32aa47052997f565d6bc
ngoxuanhieu158 <ngoxuanhieu1582005@gmail.com>
2024-11-29 08:51:55 +0700

    Add files via upload

---
+++ câu 3 (5).py
@@ -0,0 +1,11 @@
+a = "Hello Guy!"
+
+def say():
+    global a  # Sử dụng biến toàn cục
+    a = "Vinh University"  # Thay đổi giá trị của biến toàn cục
+    print(a)
+
+say()  # Gọi hàm say
+print(a)  # In ra giá trị của biến a sau khi thay đổi
+
+
