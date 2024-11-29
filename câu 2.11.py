ef9b3fce681b833ec5ccdadde38691e087706914
ngoxuanhieu158 <ngoxuanhieu1582005@gmail.com>
2024-11-29 08:48:37 +0700

    Add files via upload

---
+++ câu 2.11.py
@@ -0,0 +1,8 @@
+I=[1,'pythin',4,7]
+k=['cse',2,'gunter',8]
+m=[]
+m.append(I);
+m.append(k);
+print(m)
+d={1:1,2:k,'combine_lít':m}
+print(d)
---
+++ câu 2.3.py
@@ -0,0 +1,5 @@
+n=int(input("enter a number===>"))
+if n % 2==0:
+    print("even nember");
+else:
+    print("odd nember");
